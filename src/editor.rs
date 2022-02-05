3dd5706028949f5872dda724df1dd1484200f4b5
Shadorain <shadow@Shadowraith.fios-router.home>
2022-02-05 09:32:17 -0500

    Document and row implementations, movement as well, push for remote access on other device

--- src/editor.rs
+++ src/editor.rs
@@ -1,18 +1,26 @@
+use crate::Document;
+use crate::Row;
 use crate::Terminal;
 
-use std::io::{self, stdout, Write};
+use std::env;
 use termion::event::Key;
-use termion::input::TermRead; /* keys() on stdin */
-use termion::raw::IntoRawMode;
+
+#[derive(Default)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
 
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
+    cursor_position: Position,
+    offset: Position,
+    document: Document,
 }
 
 impl Editor {
     pub fn run (&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
         loop {
             if let Err(error) = self.refresh_screen() { die(&error); }
             if self.should_quit { break }
@@ -21,48 +29,121 @@ impl Editor {
     }
 
     pub fn default () -> Self {
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
+            cursor_position: Position::default(),
+            offset: Position::default(),
+            document,
+        }
+    }
+
+    fn draw_welcome_msg (&self) {
+        let mut welcome_msg = format!("{} -- v{}", crate::NAME, crate::VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_msg.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_msg = format!("~{}{}", spaces, welcome_msg);
+        welcome_msg.truncate(width);
+        println!("{}\r", welcome_msg);
+        // println!("{}", crate::DESCRIPTION);
+        // println!("{}", crate::REPOSITORY);
+    }
+
+    pub fn draw_row (&self, row: &Row) {
+        let width = self.terminal.size().width as usize;
+        let start = self.offset.x;
+        let end = self.offset.x + width;
+        let row = row.render(start, end);
+        println!("{}\r", row)
+    }
+
+    fn draw_rows (&self) {
+        let height = self.terminal.size().height;
+        for terminal_row in 0 .. height - 1 {
+            Terminal::clear_current_line();
+            if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
+                self.draw_row(row);
+            } else if self.document.is_empty() && terminal_row == height / 3 {
+                self.draw_welcome_msg();
+            } else { println!("~\r"); }
         }
     }
 
     fn process_keypress (&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Up | Key::Char('k')
+                | Key::Down | Key::Char('j')
+                | Key::Left | Key::Char('h')
+                | Key::Right | Key::Char('l')
+                | Key::PageUp
+                | Key::PageDown
+                | Key::End
+                | Key::Home
+                => self.move_cursor(pressed_key),
             _ => (),
         }
+        self.scroll();
         Ok(())
     }
 
-    fn draw_rows (&self) {
-        for _ in 0 .. self.terminal.size().height {
-            println!("~\r");
+    fn move_cursor (&mut self, key: Key) {
+        let Position { mut x, mut y } = self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
+        match key {
+            Key::Up    | Key::Char('k') => y = y.saturating_sub(1),
+            Key::Down  | Key::Char('j') => {
+                if y < height {
+                    y = y.saturating_add(1);
+                }
+            },
+            Key::Left  | Key::Char('h') => x = x.saturating_sub(1),
+            Key::Right | Key::Char('l') => {
+                if x < width {
+                    x = x.saturating_add(1);
+                }
+            },
+            Key::PageUp   => y = 0,
+            Key::PageDown => y = height,
+            Key::Home     => x = 0,
+            Key::End      => x = width,
+            _ => (),
         }
+        self.cursor_position = Position { x, y }
+    }
+
+    fn scroll (&mut self) {
+        let Position { x, y } = self.cursor_position;
     }
 
     fn refresh_screen (&self) -> Result<(), std::io::Error> {
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
-        if self.should_quit { println!("Goodbye.\r"); }
-        else {
+        Terminal::cursor_hide();
+        Terminal::cursor_position(&Position::default());
+        if self.should_quit {
+            Terminal::clear_screen();
+            println!("Goodbye.\r");
+        } else {
             self.draw_rows();
-            print!("{}", termion::cursor::Goto(1, 1));
+            Terminal::cursor_position(&self.cursor_position);
         }
-        io::stdout().flush()
-    }
-}
-
-
-fn read_key () -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
+        Terminal::cursor_show();
+        Terminal::flush()
     }
 }
 
 fn die (e: &std::io::Error) {
-    print!("{}", termion::clear::All);
+    Terminal::clear_screen();
     panic!("{}", e);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 #![warn(clippy::all, clippy::pedantic)]
 
+mod document;
 mod editor;
-use editor::Editor;
-
+mod row;
 mod terminal;
+
+use editor::Editor;
+pub use document::Document;
+pub use editor::Position;
+pub use row::Row;
+pub use terminal::Terminal;
+
+pub const NAME: &str = "ShadoVi"/* env!("CARGO_PKG_NAME") */;
+pub const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+pub const REPOSITORY: &str = env!("CARGO_PKG_REPOSITORY");
 
 fn main () {
     Editor::default().run();
